8179db051b078a35e9a6d231d6b449a49b9a5659
his-imag8 <ekanemdaniel60@gmail.com>
2026-04-20 22:19:49 +0100

    final update on about section smooth scroll

--- js/script.js
+++ js/script.js
@@ -28,7 +28,6 @@ const sectionObserver = new IntersectionObserver(sectionCallBack, {
 sections.forEach(function (section) {
   sectionObserver.observe(section);
 });
-<<<<<<< HEAD
 
 aboutUs.addEventListener("click", function (e) {
   e.preventDefault();
@@ -37,5 +36,3 @@ aboutUs.addEventListener("click", function (e) {
     behavior: "smooth",
   });
 });
-=======
->>>>>>> 0401c27155df70d54a78617413ed53650bfa2a12
